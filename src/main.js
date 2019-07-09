81916e3a6bb6ea4bcc6d9611efc570ce6bfe08e1
阿元啊 <799722595@qq.com>
2019-07-09 20:20:16 +0800

    Login完成

--- src/main.js
+++ src/main.js
@@ -1,8 +1,17 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+
+// 在vue-cli创建的项目中  @的前缀 代表的是根路径 src目录
+// 默认会去加载索引文件  index.js index.vue index.json  必须是index  因为只解析index
+import router from '@/router'
+Vue.use(ElementUI)
 Vue.config.productionTip = false
 
+// 实例化根实例  render() 渲染APP组件   然后挂载到#app的容器内
 new Vue({
+  router,
   render: h => h(App)
 }).$mount('#app')
---
+++ src/views/login/login.vue
@@ -0,0 +1,68 @@
+<template>
+  <div class="login-container">
+    <el-card class="login_box">
+      <img src="../../assets/images/logo_index.png" alt />
+      <el-form>
+        <el-form-item>
+          <el-input placeholder="请输入手机号"></el-input>
+        </el-form-item>
+        <el-form-item>
+          <el-input style="width:280px" placeholder="请输入验证码"></el-input>
+          <el-button style="float:right">发送验证码</el-button>
+        </el-form-item>
+        <el-form-item>
+          <el-checkbox v-model="checked"></el-checkbox>我已阅读并同意
+          <el-link :underline="false">用户协议</el-link> 和
+          <el-link :underline="false">隐私条约</el-link>
+        </el-form-item>
+        <el-form-item>
+          <el-button type="primary" style="width:100%">登录</el-button>
+        </el-form-item>
+      </el-form>
+    </el-card>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      // form: {
+      //   name: ''
+      // },
+      checked: true
+    }
+  }
+}
+</script>
+
+<style lang="less" scoped>
+.login-container {
+  width: 100%;
+  height: 100%;
+  position: absolute;
+  left: 0;
+  top: 0;
+  background: url(../../assets/images/login_bg.jpg) no-repeat center / cover;
+  .login_box {
+    width: 480px;
+    height: 360px;
+    position: absolute;
+    left: 50%;
+    top: 50%;
+    transform: translate(-50%, -50%);
+    img {
+      display: block;
+      width: 180px;
+      margin: 10px auto 30px;
+    }
+  }
+  .el-link {
+    font-size: 15px;
+    color: #409eff;
+  }
+  .el-checkbox{
+    margin-right: 5px;
+  }
+}
+</style>
